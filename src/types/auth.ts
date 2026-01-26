e3a2c3bea41d077ba91cbb8039ce818b1b4846d0
Felipe <lamediatricolor@gmail.com>
2026-01-26 00:26:09 -0500

    Agregando interfaces para autenticacion

---
+++ src/types/auth.ts
@@ -0,0 +1,12 @@
+export interface User {
+  username: string;
+  rol: string;
+}
+
+export interface AuthContextType {
+  token: string | null;
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (token: string, user: User) => void;
+  logout: () => void;
+}
