483466b6814cd1facefba199576e3ce68e488591
Felipe <lamediatricolor@gmail.com>
2026-01-26 00:56:54 -0500

    Ft: Interface para DecodedToken

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -3,10 +3,18 @@ export interface User {
   rol: string;
 }
 
+export interface DecodedToken {
+  sub: string;
+  role?: string;
+  authorities?: { authority: string }[];
+  exp: number;
+  iat: number;
+}
+
 export interface AuthContextType {
   token: string | null;
   user: User | null;
   isAuthenticated: boolean;
-  login: (token: string, user: User) => void;
+  login: (token: string) => void;
   logout: () => void;
 }
